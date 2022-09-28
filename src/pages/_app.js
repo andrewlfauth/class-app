d6ca5c398c378faeb83ceb42c130f7bc97f77da7
andrewlfauth <andrewlfauth@gmail.com>
2022-09-28 15:02:07 -0700

    Add: next-auth with primsa

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,16 @@
 import Head from 'next/head'
+import { SessionProvider } from 'next-auth/react'
 
 import '../styles/globals.css'
 import Navbar from '../components/Navbar'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, session }) {
   return (
     <>
-      <Head></Head>
-      <Navbar />
-      <Component {...pageProps} />
+      <SessionProvider session={session}>
+        <Navbar />
+        <Component {...pageProps} />
+      </SessionProvider>
     </>
   )
 }
--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import prisma from '../../../prismadb'
+import { prisma } from '../../../prismadb'
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
