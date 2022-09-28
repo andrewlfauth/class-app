68e796893db990a91baa0ef778644da11dbfbd54
andrewlfauth <andrewlfauth@gmail.com>
2022-09-27 17:51:52 -0700

    Styling

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,9 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar />
-      <div className='py-20 lg:py-32 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto'>
-        <Component {...pageProps} />
-      </div>
+      <Component {...pageProps} />
     </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,19 +3,6 @@ import { useRouter } from 'next/router'
 import Link from 'next/link'
 
 export default function Home() {
-  const router = useRouter()
-
-  // async function handleSubmit(e) {
-  //   const data = getFormData(e)
-  //   await fetch('/api/create-teacher', {
-  //     method: 'POST',
-  //     body: JSON.stringify(data),
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //   })
-  // }
-
   return (
     <div>
       <Head>
@@ -27,24 +14,26 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className='flex flex-col items-center'>
-        <h1 className='text-4xl md:text-6xl max-w-3xl mx-auto font-bold text-center tracking-tight'>
-          Managing your classroom just became easy
-        </h1>
+      <div>
+        <main className='flex flex-col items-center py-20 lg:py-32 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto'>
+          <h1 className='text-4xl md:text-6xl max-w-3xl mx-auto font-bold text-center tracking-tight'>
+            Managing your classroom just became easy
+          </h1>
 
-        <div className='mt-10 flex space-x-4'>
-          <Link href='register'>
-            <a className='bg-purple-500 text-gray-100 rounded py-4 text-xl font-semibold w-44 text-center block'>
-              Get Started
-            </a>
-          </Link>
-          <Link href='login'>
-            <a className='bg-gray-200 text-gray-600 rounded py-4 text-xl font-semibold w-44 text-center block'>
-              Login
-            </a>
-          </Link>
-        </div>
-      </main>
+          <div className='mt-10 flex space-x-4'>
+            <Link href='register'>
+              <a className='bg-purple-500 text-gray-100 rounded py-4 text-xl font-semibold w-44 text-center block'>
+                Get Started
+              </a>
+            </Link>
+            <Link href='login'>
+              <a className='bg-gray-200 text-gray-600 rounded py-4 text-xl font-semibold w-44 text-center block'>
+                Login
+              </a>
+            </Link>
+          </div>
+        </main>
+      </div>
     </div>
   )
 }
