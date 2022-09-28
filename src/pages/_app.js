27661886ccde11d8220971ad3e553d7b079255e9
andrewlfauth <andrewlfauth@gmail.com>
2022-09-28 12:28:54 -0700

    Add: favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,14 @@
+import Head from 'next/head'
+
 import '../styles/globals.css'
 import Navbar from '../components/Navbar'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel='icon' href='/favicon.png' />
+      </Head>
       <Navbar />
       <Component {...pageProps} />
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,23 +11,22 @@ export default function Home() {
           name='description'
           content='App for teachers to help manage classrooms'
         />
-        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <div>
-        <main className='flex flex-col items-center py-20 lg:py-32 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto'>
-          <h1 className='text-4xl md:text-6xl max-w-3xl mx-auto font-bold text-center tracking-tight'>
+        <main className='flex flex-col items-center px-4 py-20 mx-auto lg:py-32 sm:px-6 lg:px-8 max-w-7xl'>
+          <h1 className='max-w-3xl mx-auto text-4xl font-bold tracking-tight text-center md:text-6xl'>
             Managing your classroom just became easy
           </h1>
 
-          <div className='mt-10 flex space-x-4'>
+          <div className='flex mt-10 space-x-4'>
             <Link href='register'>
-              <a className='bg-purple-500 text-gray-100 rounded py-4 text-xl font-semibold w-44 text-center block'>
+              <a className='block py-4 text-xl font-semibold text-center text-gray-100 bg-purple-500 rounded w-44'>
                 Get Started
               </a>
             </Link>
             <Link href='login'>
-              <a className='bg-gray-200 text-gray-600 rounded py-4 text-xl font-semibold w-44 text-center block'>
+              <a className='block py-4 text-xl font-semibold text-center text-gray-600 bg-gray-200 rounded w-44'>
                 Login
               </a>
             </Link>
