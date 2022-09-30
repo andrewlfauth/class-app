267dcc391205a7c5bbc2e6367cffccc078a07094
andrewlfauth <andrewlfauth@gmail.com>
2022-09-30 16:40:54 -0700

    Add: AuthWrapper component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,17 @@
-import Head from 'next/head'
 import { SessionProvider } from 'next-auth/react'
 
 import '../styles/globals.css'
 import Navbar from '../components/Navbar'
+import AuthWrapper from '../components/AuthWrapper'
 
 function MyApp({ Component, pageProps, session }) {
   return (
     <>
       <SessionProvider session={session}>
-        <Navbar />
-        <Component {...pageProps} />
+        <AuthWrapper>
+          <Navbar />
+          <Component {...pageProps} />
+        </AuthWrapper>
       </SessionProvider>
     </>
   )
