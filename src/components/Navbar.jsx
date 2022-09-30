e07045b6e3af4a5ba4947bee2c4874e834fdc992
andrewlfauth <andrewlfauth@gmail.com>
2022-09-29 18:08:27 -0700

    Add: logo with sign in/out functionality and user img

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,18 +6,48 @@ function Navbar() {
   return (
     <div className='bg-purple-500'>
       <nav className='px-4 flex justify-between items-center sm:px-6 lg:px-8 mx-auto max-w-7xl py-2'>
+        <div className='flex items-end space-x-1'>
+          <Image
+            src='https://res.cloudinary.com/dpnkrz8c8/image/upload/v1664499443/classroom/favicon_ojxbho.png'
+            alt='logo'
+            width={30}
+            height={30}
+          />
+          <span className='text-gray-100 font-bold tracking-tight text-lg'>
+            Classrooms
+          </span>
+        </div>
+
         {session ? (
-          <button onClick={() => signOut()}>Sign out</button>
+          <div className='flex items-center'>
+            <button
+              className='text-white rounded py-1 w-24 mr-4 font-semibold bg-purple-900'
+              onClick={() => signOut()}
+            >
+              Sign out
+            </button>
+            <Image
+              className='rounded-full'
+              src={session?.user.image}
+              width={30}
+              height={30}
+              alt='profile'
+            />
+          </div>
         ) : (
           <button onClick={() => signIn()}>Sign in</button>
         )}
-        {session && (
+        {/* {session && (
           <div>
-            <h1>{session?.user.name}</h1>
-            <h1>{session?.user.email}</h1>
-            <Image src={session?.user.image} width={20} height={20} />
+            <Image
+              className='rounded-full'
+              src={session?.user.image}
+              width={30}
+              height={30}
+              alt='profile'
+            />
           </div>
-        )}
+        )} */}
       </nav>
     </div>
   )
